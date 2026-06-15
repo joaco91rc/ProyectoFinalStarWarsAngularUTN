12c8da6ab1ebe6f950c5e4333a53b0bf1c22cfaa
Joaco Alvarez <joaquinaalvarez91@gmail.com>
2026-06-15 01:44:44 -0300

    Avances

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,5 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('galactic-lightsaber-forge');
+  protected readonly title = signal("Guardian's Forge");
 }
